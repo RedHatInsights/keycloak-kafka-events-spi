f8feccc655eb34ee41d049134a2d3c627800ea9f
Doug Donahue <ddonahue@redhat.com>
2024-09-10 14:36:25 -0400

    initial src build out

---
+++ src/main/java/com/redhat/keycloak/events/KafkaProducerInterface.java
@@ -0,0 +1,12 @@
+package com.redhat.keycloak.events;
+
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.Producer;
+
+public interface KafkaProducerInterface {
+
+    Producer<String, String> createProducer(String clientId, String bootstrapServer,
+                                            Map<String, Object> optionalProperties);
+
+}
---
+++ src/test/KafkaMockFactory.java
@@ -0,0 +1,18 @@
+package com.redhat.keycloak.events;
+
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+class KafkaMockFactory implements KafkaProducerFactory {
+
+    @Override
+    public Producer<String, String> createProducer(String clientId, String bootstrapServer,
+                                                   Map<String, Object> optionalProperties) {
+        return new MockProducer<>(true, new StringSerializer(), new StringSerializer());
+    }
+
+}
+
